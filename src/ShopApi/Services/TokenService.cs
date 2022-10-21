f6641108dcecfffbad2dd50482e88a579625021c
Renan Bernardo <renan.brnrd@gmail.com>
2022-10-21 08:56:45 -0300

    Gera, obtém e persiste o Refresh Token

--- src/ShopApi/Services/TokenService.cs
+++ src/ShopApi/Services/TokenService.cs
@@ -1,5 +1,6 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
 using ShopApi.Interfaces.Settings;
@@ -27,10 +28,72 @@ public TokenService(ISettingsService settingsService)
                 new Claim(ClaimTypes.Name, user.Username.ToString()),
                 new Claim(ClaimTypes.Role, user.Role.ToString())
             }),
-            Expires = DateTime.UtcNow.AddHours(8),
+            Expires = DateTime.UtcNow.AddHours(2),
             SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
         };
         var token = tokenHandler.CreateToken(tokenDescriptor);
         return tokenHandler.WriteToken(token);
+    }
+
+    public string GenerateToken(IEnumerable<Claim> claims)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_settingsService.GetAuthSettings().Secret);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddHours(2),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                                            SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+
+    public static string GenerateRefreshToken()
+    {
+        var randomNumber = new byte[32];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(randomNumber);
+        return Convert.ToBase64String(randomNumber);
+    }
+
+    public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+    {
+        var tokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_settingsService.GetAuthSettings().Secret)),
+            ValidateLifetime = false
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var securityToken);
+        if (securityToken is not JwtSecurityToken jwtSecurityToken ||
+            !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, 
+            StringComparison.InvariantCultureIgnoreCase))
+            throw new SecurityTokenException("Invalid Token");
+        
+        return principal;
+    }
+
+    private static List<(string, string)> _refreshTokens = new();
+
+    public static void SaveRefreshToken(string username, string refreshtoken)
+    {
+        _refreshTokens.Add(new (username, refreshtoken));
+    }
+
+    public static string GetRefreshToken(string username)
+    {
+        return _refreshTokens.FirstOrDefault(x => x.Item1 == username).Item2;
+    }
+
+    public static void DeleteRefreshToken(string username, string refreshtoken)
+    {
+        var item = _refreshTokens.FirstOrDefault(x => x.Item1 == username && x.Item2 == refreshtoken);
+        _refreshTokens.Remove(item);
     }
 }
